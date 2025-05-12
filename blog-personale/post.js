0f8801042f4e0a5bf779720d636882fb1f11c11d
dannyarena <arenadanny71@gmail.com>
2025-05-12 20:47:31 +0200

    add posts data structure to post.js

---
+++ blog-personale/post.js
@@ -0,0 +1,48 @@
+const posts = [
+    {
+        id: 1,
+        titolo: "Il ciambellone",
+        contenuto: "Il ciambellone è un dolce tipico della tradizione italiana."
+        immagine: "images/ciambellone.jpeg",
+        tags: ["dolci", "ciambellone", "tradizione"],
+        
+    },
+
+    {
+        id: 2,
+        titolo: "cracker barbabietola",
+        contenuto: "Il cracker alla barbabietola è un dolce tipico della tradizione italiana"
+        immagine: "images/cracker_barbabietola.jpeg",
+        tags: ["dolci", "cracker", "tradizione"],
+        
+    },
+
+    {
+        id: 3,
+        titolo: "Pane fritto dolce",
+        contenuto: "Il pane fritto dolce è un dolce tipico della tradizione italiana"
+        immagine: "images/pane_fritto_dolce.jpeg",
+        tags: ["dolci", "pane", "tradizione"],
+        
+    },
+
+    {
+        id: 4,
+        titolo: "pasta barbabietola",
+        contenuto: "La pasta alla barbabietola è un dolce tipico della tradizione italiana"
+        immagine: "images/pasta_barbabietola.jpeg",
+        tags: ["dolci", "pasta", "tradizione"],
+        
+    },
+
+    {
+        id: 5,
+        titolo: "torta paesana",
+        contenuto: "La torta paesana è un dolce tipico della tradizione italiana"
+        immagine: "images/torta_paesana.jpeg",
+        tags: ["dolci", "torta", "tradizione"],
+        
+    }
+];
+
+module.exports = posts;
